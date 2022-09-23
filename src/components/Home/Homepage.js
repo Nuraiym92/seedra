2ec7de32c5ebf53ced8b157e423ec2b6eabfc2cf
Nuraiym92 <nuraiym.zhyrgalbekova.kk@gmail.com>
2022-09-23 12:56:36 +0630

    seedra

---
+++ src/components/Home/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from "./Navbar/Navbar"
+function Home() {
+  return (
+<>
+<Navbar/>
+</>
+  )
+}
+
+export default Home
---
+++ src/components/Home/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from "./../../../images/logo.svg"
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="menu">
+        
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
